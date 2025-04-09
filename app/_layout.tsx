7d035a3dd3ce7f5cc18d7ab81ab6635107b9d76a
r-kojima <kojimar24@gmail.com>
2025-04-09 09:08:02 +0900

    add root layout

---
+++ app/_layout.tsx
@@ -0,0 +1,44 @@
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Slot, SplashScreen } from "expo-router";
+import React, { useEffect } from "react";
+import { useColorScheme } from "react-native";
+import { PaperProvider, MD3DarkTheme, MD3LightTheme } from "react-native-paper";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+  const colorScheme = useColorScheme();
+  const [loaded] = useFonts({
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null; // フォントロード中は何も表示しない
+  }
+
+  const navigationTheme = colorScheme === "dark" ? DarkTheme : DefaultTheme;
+  const paperTheme = colorScheme === "dark" ? MD3DarkTheme : MD3LightTheme;
+
+  return (
+    <SafeAreaProvider>
+      <ThemeProvider value={navigationTheme}>
+        <PaperProvider theme={paperTheme}>
+          <Slot />
+        </PaperProvider>
+      </ThemeProvider>
+    </SafeAreaProvider>
+  );
+}
